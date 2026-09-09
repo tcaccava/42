dc4bded046e9bc7c2d0535987114e6f4ae4d7b02
Tobia Caccavale <tcaccava@c2r5s1.42roma.it>
2026-09-09 23:27:42 +0200

    dr quine is a pain in the ass

--- DR_QUINE/colleen.c
+++ DR_QUINE/colleen.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-#include <unistd.h>
-void call_me(){
-    write(1,"tobia",5);
-}
+#define S "#include <stdio.h>\nint main() {\n\tprintf(S);\n}"
 
 int main() {
-    char *previous = "#include <stdio.h>\n#include <unistd.h>\nvoid call_me(){\n\twrite(1,\22tobia\22,5);\n}\n";
-    char *main = "int main() {\n\tchar *previous = ";
-    printf(" %s", passive,passive);
+    printf(S);
 }
