c1efa2c27fcca58fc42d6d6b88a161e306917b29
Tobia Caccavale <tcaccava@c3r6s7.42roma.it>
2026-09-19 23:02:55 +0200

    sully refactored

--- DR_QUINE/Sully.c
+++ DR_QUINE/Sully.c
@@ -4,42 +4,39 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <string.h>
 
-int main(int argc,char **argv,char **envp)
+int main(int argc, char **argv, char **envp)
 {
-    static unsigned short runs = 0;
-    if(runs > 5)
+    static short runs = 5;
+    int is_the_real_sully = !(strcmp(__FILE__, "Sully.c"));
+    if (!is_the_real_sully)
+        runs--;
+    if (runs < 0)
         return 0;
     char filename[] = "Sully_ .c";
     filename[6] = (unsigned char)(runs + 48);
     int file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
     if (file < 0)
-        printf("Error opening the file");
-    char *src_code = "#include <stdio.h>%1$c#include <unistd.h>%1$c#include <fcntl.h>%1$c#include <stdlib.h>%1$c#include <sys/wait.h>%1$c#include <sys/types.h>%1$c%1$cint main(int argc,char **argv,char **envp)%1$c{%1$c    static unsigned short runs = %4$d;%1$c    if(runs > 5)%1$c        return 0;%1$c    char filename[] = %2$cSully_ .c%2$c;%1$c    filename[6] = (unsigned char)(runs + 48);%1$c    int file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);%1$c    if (file < 0)%1$c        printf(%2$cError opening the file%2$c);%1$c    char *src_code = %2$c%3$s%2$c;%1$c    dprintf(file,src_code,10,34,src_code,runs);%1$c    pid_t pid = fork();%1$c    char output[8] = %2$cSully%2$c;%1$c    if (runs > 0)%1$c    {%1$c        output[5] = '_';%1$c        output[6] = (unsigned char)(runs + 48);%1$c        output[7] = 0;%1$c    }%1$c    int exit_status;%1$c    if (pid == 0)%1$c    {%1$c        execve(%2$c/usr/bin/gcc%2$c, (char *[]){%2$cgcc%2$c, filename, %2$c-o%2$c, output, NULL}, envp);%1$c        exit(1);%1$c    }%1$c    else if (pid > 0)%1$c        waitpid(pid, &exit_status, 0);%1$c    if (WIFEXITED(exit_status))%1$c    {%1$c        int exit_code = WEXITSTATUS(exit_status);%1$c        if (exit_code == 0)%1$c        {%1$c            runs += 1;%1$c            main(argc,argv,envp);%1$c        }%1$c    }%1$c}%1$c";
-    dprintf(file,src_code,10,34,src_code,runs);
-    pid_t pid = fork();
-    char output[8] = "Sully";
-    if (runs > 0)
+        return 1;
+    char *src_code = "#include <stdio.h>%1$c#include <unistd.h>%1$c#include <fcntl.h>%1$c#include <stdlib.h>%1$c#include <sys/wait.h>%1$c#include <sys/types.h>%1$c#include <string.h>%1$c%1$cint main(int argc, char **argv, char **envp)%1$c{%1$c    static short runs = %4$d;%1$c    int is_the_real_sully = !(strcmp(__FILE__, %2$cSully.c%2$c));%1$c    if (!is_the_real_sully)%1$c        runs--;%1$c    if (runs < 0)%1$c        return 0;%1$c    char filename[] = %2$cSully_ .c%2$c;%1$c    filename[6] = (unsigned char)(runs + 48);%1$c    int file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);%1$c    if (file < 0)%1$c        return 1;%1$c    char *src_code = %2$c%3$s%2$c;%1$c    dprintf(file, src_code, 10, 34, src_code, runs);%1$c    close(file);%1$c    char exe_name[10] = %2$c./Sully_ %2$c;%1$c    exe_name[8] = (unsigned char)(runs + 48);%1$c    pid_t pid_gcc = fork();%1$c    if (!pid_gcc)%1$c    {%1$c        execve(%2$c/usr/bin/gcc%2$c, (char *[]){%2$cgcc%2$c, filename, %2$c-o%2$c, exe_name + 2, NULL}, envp);%1$c        exit(1);%1$c    }%1$c    waitpid(pid_gcc, NULL, 0);%1$c    if (!fork())%1$c    {%1$c        char *exec_args[] = {exe_name, NULL};%1$c        execve(exe_name, exec_args, envp);%1$c        exit(1);%1$c    }%1$c    wait(NULL);%1$c    return 0;%1$c}%1$c";
+    dprintf(file, src_code, 10, 34, src_code, runs);
+    close(file);
+    char exe_name[10] = "./Sully_ ";
+    exe_name[8] = (unsigned char)(runs + 48);
+    pid_t pid_gcc = fork();
+    if (!pid_gcc)
     {
-        output[5] = '_';
-        output[6] = (unsigned char)(runs + 48);
-        output[7] = 0;
-    }
-    int exit_status;
-    if (pid == 0)
-    {
-        execve("/usr/bin/gcc", (char *[]){"gcc", filename, "-o", output, NULL}, envp);
+        execve("/usr/bin/gcc", (char *[]){"gcc", filename, "-o", exe_name + 2, NULL}, envp);
         exit(1);
     }
-    else if (pid > 0)
-        waitpid(pid, &exit_status, 0);
-    if (WIFEXITED(exit_status))
+    waitpid(pid_gcc, NULL, 0);
+    if (!fork())
     {
-        int exit_code = WEXITSTATUS(exit_status);
-        if (exit_code == 0)
-        {
-            runs += 1;
-            main(argc,argv,envp);
-        }
+        char *exec_args[] = {exe_name, NULL};
+        execve(exe_name, exec_args, envp);
+        exit(1);
     }
+    wait(NULL);
+    return 0;
 }
--- DR_QUINE/wrong_ways.c
+++ DR_QUINE/wrong_ways.c
@@ -1,7 +1,90 @@
-//questo e' il massimo livello di autoriferimento che un blocco di codice monolitico
-// puo' raggiungere senza distinzione tra componente attiva e passiva
+
+// ==============================================================================
+// IL PARADOSSO DELL'INFINITO REGRESSO (Il limite del codice monolitico)
+
+// Questo e' il massimo livello di autoriferimento che un blocco di codice 
+// monolitico puo' raggiungere senza distinzione tra componente attiva e passiva.
+
 #include <stdio.h>
 #define S "#include <stdio.h>\n#define S \nint main() {\n\tprintf(S);\n}\n"
-int main() {
+
+void monolithic_limit() {
     printf(S);
+}
+
+/*
+ ANALISI MATEMATICA (Perche' fallisce):
+ Se eseguo la funzione, l'output sara':
+ #include <stdio.h>
+ #define S 
+ int main() {
+    printf(S);
+  } 
+ Manca la stringa stessa assegnata alla macro S nella riga 2.
+ Per far sì che il codice stampi la propria stringa, la stringa 'S' dovrebbe 
+ contenere se stessa. Se provassimo ad aggiungerla, S conterrebbe S, che a sua 
+ volta contiene S, generando una stringa di lunghezza infinita. 
+ Questo è il limite invalicabile dimostrato dal Teorema di Ricorsione di Kleene.
+ L'unico modo per risolverlo è scindere il programma:
+ - Dati Passivi (Data): una stringa formattabile.
+ - Codice Attivo (Code): istruzioni che usano i dati passivi DUE VOLTE 
+   (una volta interpretandoli come codice eseguibile, una volta come dati grezzi).
+ */
+
+
+// ==============================================================================
+// IL FALSO QUINE (tramite I/O, argv e __FILE__)
+
+// Molti credono che aprire il file sorgente dal disco sia un Quine. 
+// E' un approccio perdente e concettualmente nullo.
+
+#include <stdlib.h>
+
+void pseudo_quine_argv_file(int argc, char **argv) {
+    
+    // TENTATIVO 1: Usare il nome dell'eseguibile (argv[0]) per dedurre il file .c
+    // char filename[256];
+    // snprintf(filename, sizeof(filename), "%s.c", argv[0]);
+    // FILE *file = fopen(filename, "r");
+
+    // TENTATIVO 2: Usare la macro di precompilazione __FILE__
+    FILE *file = fopen(__FILE__, "r");
+
+    if (file) {
+        int c;
+        while ((c = fgetc(file)) != EOF) {
+            putchar(c);
+        }
+        fclose(file);
+    }
+}
+
+/*
+ Perche' e' scientificamente invalido ed equivale ad un cheat:
+ - Definizione di input: un vero Quine è una Macchina di Turing definita
+   "senza input". Deve generare il proprio codice a partire esclusivamente dal 
+   suo stato interno (la memoria del binario). Aprire un file significa leggere 
+   un input fornito dal filesystem del Sistema Operativo.
+
+ - Rottura del paradigma di autoreplicazione: l'informazione "genetica" del 
+   programma non risiede nel programma stesso (come in un virus), ma nel disco 
+   ospite. Se compilo questo file in anti_quine, lo sposto in un'altra cartella 
+   ed elimino il sorgente anti_quine.c, il binario si rompe e smette di funzionare. 
+   Un VERO Quine (che usa dati in memoria) stamperà il sorgente .c indipendentemente 
+   dal fatto che esista sul disco o meno.
+ */
+
+
+// ==============================================================================
+// MAIN DI TEST
+// ==============================================================================
+
+int main(int argc, char **argv) {
+    printf("--- RISULTATO DEL CODICE MONOLITICO ---\n");
+    monolithic_limit();
+    
+    printf("\n--- RISULTATO DEL CHEAT ---\n");
+    pseudo_quine_argv_file(argc, argv);
+    
+    return 0;
 }
